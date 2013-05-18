9af5fe35588dccabd43932f8a19e2f705cdc85b4
Jan Horava <czechinthemail@gmail.com>
2013-05-18 00:03:22 -0700

    improved formatting

--- warped.js
+++ warped.js
@@ -1,6 +1,11 @@
 function draw(){
     if(settings[3]){/*clear?*/
-        buffer.clearRect(0,0,width,height)
+        buffer.clearRect(
+            0,
+            0,
+            width,
+            height
+        )
     }
 
     buffer.lineWidth = settings[0];
@@ -15,7 +20,12 @@ function draw(){
     }while(i--)
 
     if(settings[3]){/*clear?*/
-        canvas.clearRect(0,0,width,height)
+        canvas.clearRect(
+            0,
+            0,
+            width,
+            height
+        )
     }
     canvas.drawImage(get('buffer'),0,0)
 }
@@ -99,7 +109,7 @@ function setmode(newmode){
         buffer = 0;
         canvas = 0;
 
-        get('page').innerHTML = '<div style="border-right:8px solid #222;display:inline-block;text-align:left;vertical-align:top"><div class=c><b>Warped</b></div><hr><div class=c style=color:#f00>SEIZURE WARNING!<br>FLASHING LIGHTS!</div><hr><div class=c><a onclick=setmode(1)>Lines</a></div><hr><div class=c><input id=number-of-lines size=1 type=text value='+settings[1]+'>Lines<br><input id=line-width size=1 type=text value='+settings[0]+'>Line Width<br><label><input '+(settings[4]?'checked ':'')+'id=mouse-lock type=checkbox>Mouse Lock</label></div></div><div style=display:inline-block;text-align:left><div class=c><input disabled size=3 style=border:0 type=text value=ESC>Main Menu<br><input id=randomize-key maxlength=1 size=3 type=text value='+settings[2]+'>Randomize</div><hr><div class=c><label><input '+(settings[3]?'checked ':'')+'id=clear type=checkbox>Clear</label><br><a onclick="if(confirm(\'Reset settings?\')){get(\'clear\').checked=get(\'line-width\').value=get(\'mouse-lock\').checked=1;get(\'randomize-key\').value=\'R\';get(\'number-of-lines\').value=100;save();setmode(0)}">Reset Settings</a></div></div>'
+        get('page').innerHTML = '<div style="border-right:8px solid #222;display:inline-block;text-align:left;vertical-align:top"><div class=c><b>Warped</b></div><hr><div class=c style=color:#f00>SEIZURE WARNING!<br>FLASHING LIGHTS!</div><hr><div class=c><a onclick=setmode(1)>Lines</a></div><hr><div class=c><input id=number-of-lines size=1 type=text value='+settings[1]+'>Lines<br><input id=line-width size=1 type=text value='+settings[0]+'>Line Width<br><label><input '+(settings[4] ? 'checked ' : '')+'id=mouse-lock type=checkbox>Mouse Lock</label></div></div><div style=display:inline-block;text-align:left><div class=c><input disabled size=3 style=border:0 type=text value=ESC>Main Menu<br><input id=randomize-key maxlength=1 size=3 type=text value='+settings[2]+'>Randomize</div><hr><div class=c><label><input '+(settings[3] ? 'checked ' : '')+'id=clear type=checkbox>Clear</label><br><a onclick="if(confirm(\'Reset settings?\')){get(\'clear\').checked=get(\'line-width\').value=get(\'mouse-lock\').checked=1;get(\'randomize-key\').value=\'R\';get(\'number-of-lines\').value=100;save();setmode(0)}">Reset Settings</a></div></div>'
     }
 }
 var buffer = 0;
@@ -113,9 +123,9 @@ var mode = 0;
 var mouse_x = 0;
 var mouse_y = 0;
 var settings = [
-    ls.getItem('warped0')===null?1:parseInt(ls.getItem('warped0')),/*line width*/
-    ls.getItem('warped1')===null?100:parseInt(ls.getItem('warped1')),/*number of lines*/
-    ls.getItem('warped2')===null?'R':ls.getItem('warped2'),/*randomize key*/
+    ls.getItem('warped0')===null ? 1 : parseInt(ls.getItem('warped0')),/*line width*/
+    ls.getItem('warped1')===null ? 100 : parseInt(ls.getItem('warped1')),/*number of lines*/
+    ls.getItem('warped2')===null ? 'R' : ls.getItem('warped2'),/*randomize key*/
     ls.getItem('warped3')===null,/*clear?*/
     ls.getItem('warped4')===null/*mouse lock?*/
 ];
@@ -128,8 +138,8 @@ setmode(0);
 
 window.onkeydown = function(e){
     if(mode>0){
-        i = window.event?event:e;
-        i = i.charCode?i.charCode:i.keyCode;
+        i = window.event ? event : e;
+        i = i.charCode ? i.charCode : i.keyCode;
         if(String.fromCharCode(i)===settings[2]){/*randomize key*/
             randomize_lines()
         }else if(i===27){/*ESC*/
