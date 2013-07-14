0a5400e70e53ae2c37f29010c3c117c20c420275
Jan Horava <czechinthemail@gmail.com>
2013-07-13 21:16:02 -0700

    improved formatting

--- js/warped.js
+++ js/warped.js
@@ -1,5 +1,5 @@
 function draw(){
-    if(settings[3]){/* clear? */
+    if(settings[3]){// clear?
         buffer.clearRect(
             0,
             0,
@@ -8,10 +8,11 @@ function draw(){
         );
     }
 
-    /* circles */
+    i = objs.length - 1;
+
+    // circles mode
     if(mode == 1){
         buffer.lineWidth = settings[0];
-        i = objs.length - 1;
         do{
             buffer.beginPath();
             buffer.moveTo(
@@ -29,9 +30,8 @@ function draw(){
             buffer.stroke();
         }while(i--);
 
-    /* rectangles */
+    // rectangles mode
     }else{
-        i = objs.length - 1;
         do{
             buffer.fillStyle = 'rgb(' + objs[i][2] + ', '
                                       + objs[i][3] + ', '
@@ -45,7 +45,7 @@ function draw(){
         }while(i--);
     }
 
-    if(settings[3]){/* clear? */
+    if(settings[3]){// clear?
         canvas.clearRect(
             0,
             0,
@@ -67,9 +67,9 @@ function get(i){
 function randomize_objs(){
     objs = [];
 
-    i = settings[1] - 1;/* number of objs */
+    i = settings[1] - 1;// number of objs
     do{
-        /* create line with start_x, start_y, and random rgb color */
+        // create line with start_x, start_y, and random rgb color
         objs.push([
             random_number(width),
             random_number(height),
@@ -107,7 +107,7 @@ function resize(){
 }
 
 function save(){
-    /* save settings into localStorage if differ from default */
+    // save settings into localStorage if differ from default
 
     if(get('randomize-key').value === 'R'){
         ls.removeItem('warped-2');
@@ -162,7 +162,7 @@ function save(){
 function setmode(newmode){
     mode = newmode;
 
-    /* new visualization mode */
+    // new visualization mode
     if(mode > 0){
         save();
 
@@ -172,15 +172,15 @@ function setmode(newmode){
 
         resize();
 
-    /* main menu mode */
+    // main menu mode
     }else{
         buffer = 0;
         canvas = 0;
 
-        get('page').innerHTML = '<div style="border-right:8px solid #222;display:inline-block;text-align:left;vertical-align:top"><div class=c><b>Warped</b></div><hr><div class=c style=color:#f00>SEIZURE WARNING!<br>FLASHING COLORS!</div><hr><div class=c><ul><li><a onclick=setmode(1)>Lines</a><li><a onclick=setmode(2)>Rectangles</a></ul></div><hr><div class=c><input id=line-width size=1 type=text value='
+        get('page').innerHTML = '<div style="border-right:8px solid #222;display:inline-block;text-align:left;vertical-align:top"><div class=c><b>Warped</b></div><hr><div class=c style=color:#f00>SEIZURE WARNING!<br>FLASHING COLORS!</div><hr><div class=c><ul><li><a onclick=setmode(1)>Lines</a><li><a onclick=setmode(2)>Rectangles</a></ul></div><hr><div class=c><input id=line-width size=1 value='
             + settings[0] + '>Line Width<br><label><input '
-            + (settings[4] ? 'checked ' : '') + 'id=mouse-lock type=checkbox>Mouse Lock</label><br><input id=number-of-objs size=1 type=text value='
-            + settings[1] + '>Objects</div></div><div style=display:inline-block;text-align:left><div class=c><input disabled size=3 style=border:0 type=text value=ESC>Main Menu<br><input id=randomize-key maxlength=1 size=3 type=text value='
+            + (settings[4] ? 'checked ' : '') + 'id=mouse-lock type=checkbox>Mouse Lock</label><br><input id=number-of-objs size=1 value='
+            + settings[1] + '>Objects</div></div><div style=display:inline-block;text-align:left><div class=c><input disabled size=3 style=border:0 value=ESC>Main Menu<br><input id=randomize-key maxlength=1 size=3 value='
             + settings[2] + '>Randomize</div><hr><div class=c><label><input '
             + (settings[3] ? 'checked ' : '') + 'id=clear type=checkbox>Clear</label><br><a onclick="if(confirm(\'Reset settings?\')){get(\'clear\').checked=get(\'line-width\').value=get(\'mouse-lock\').checked=1;get(\'randomize-key\').value=\'R\';get(\'number-of-lines\').value=100;save();setmode(0)}">Reset Settings</a></div></div>';
     }
@@ -197,11 +197,11 @@ var mode = 0;
 var mouse_x = 0;
 var mouse_y = 0;
 var settings = [
-    ls.getItem('warped-0') === null ?   1 : parseInt(ls.getItem('warped-0')),/* line width */
-    ls.getItem('warped-1') === null ? 100 : parseInt(ls.getItem('warped-1')),/* number of lines */
-    ls.getItem('warped-2') === null ? 'R' : ls.getItem('warped-2'),/* randomize key */
-    ls.getItem('warped-3') === null,/* clear? */
-    ls.getItem('warped-4') === null/* mouse lock? */
+    ls.getItem('warped-0') === null ?   1 : parseInt(ls.getItem('warped-0')),// line width
+    ls.getItem('warped-1') === null ? 100 : parseInt(ls.getItem('warped-1')),// number of lines
+    ls.getItem('warped-2') === null ? 'R' : ls.getItem('warped-2'),// randomize key
+    ls.getItem('warped-3') === null,// clear?
+    ls.getItem('warped-4') === null// mouse lock?
 ];
 var x = 0;
 var width = 0;
@@ -215,10 +215,10 @@ window.onkeydown = function(e){
         i = window.event ? event : e;
         i = i.charCode ? i.charCode : i.keyCode;
 
-        if(String.fromCharCode(i) === settings[2]){/* randomize key */
+        if(String.fromCharCode(i) === settings[2]){// randomize key
             randomize_lines();
 
-        }else if(i === 27){/* ESC */
+        }else if(i === 27){// ESC
             setmode(0);
         }
     }
@@ -227,7 +227,7 @@ window.onkeydown = function(e){
 window.onmousedown = function(e){
     if(mode > 0){
         e.preventDefault();
-        if(!settings[4]){/* mouse not locked */
+        if(!settings[4]){// mouse not locked
             mouse_x = e.pageX;
             mouse_y = e.pageY;
 
@@ -237,7 +237,7 @@ window.onmousedown = function(e){
 };
 
 window.onmousemove = function(e){
-    if(mode > 0 && settings[4]){/* mouse locked */
+    if(mode > 0 && settings[4]){// mouse locked
         mouse_x = e.pageX;
         mouse_y = e.pageY;
 
