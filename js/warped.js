df136e7ae053f52e853ef96085f3be2e9b89f34f
Jan Horava <czechinthemail@gmail.com>
2015-03-15 18:08:52 -0700

    removed extra title, improved clarity

--- js/warped.js
+++ js/warped.js
@@ -112,10 +112,8 @@ function resize(){
     randomize_objects();
 }
 
-// Save settings into window.localStorage
-//   if they differ from default settings.
+// Save settings into window.localStorage if they differ from default.
 function save(){
-    // Save clear setting.
     if(document.getElementById('clear').checked){
         window.localStorage.removeItem('Warped.htm-clear');
         settings['clear'] = true;
@@ -128,7 +126,6 @@ function save(){
         );
     }
 
-    // Save line-width setting.
     if(isNaN(document.getElementById('line-width').value)
       || document.getElementById('line-width').value < 2){
         window.localStorage.removeItem('Warped.htm-line-width');
@@ -142,7 +139,6 @@ function save(){
         );
     }
 
-    // Save randomization key setting.
     if(document.getElementById('randomize-key').value === 'R'){
         window.localStorage.removeItem('Warped.htm-randomize-key');
         settings['randomize-key'] = 'R';
@@ -155,7 +151,6 @@ function save(){
         );
     }
 
-    // Save mouse-lock setting.
     if(document.getElementById('mouse-lock').checked){
         window.localStorage.removeItem('Warped.htm-mouse-lock');
         settings['mouse-lock'] = true;
@@ -168,7 +163,6 @@ function save(){
         );
     }
 
-    // Save number-of-objects setting.
     if(document.getElementById('number-of-objects').value === 100
       || isNaN(document.getElementById('number-of-objects').value)
       || document.getElementById('number-of-objects').value < 1){
@@ -203,7 +197,7 @@ function setmode(newmode){
         buffer = 0;
         canvas = 0;
 
-        document.getElementById('page').innerHTML = '<div style=display:inline-block;text-align:left;vertical-align:top><div class=c><b>Warped.htm</b></div><hr><div class=c style=color:#f00>SEIZURE WARNING!<br>FLASHING COLORS!</div><hr><div class=c><ul><li><a onclick=setmode(3)>Both</a><li><a onclick=setmode(1)>Lines</a><li><a onclick=setmode(2)>Rectangles</a></ul></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c><input disabled style=border:0 value=ESC>Main Menu<br><input id=randomize-key maxlength=1 value='
+        document.getElementById('page').innerHTML = '<div style=display:inline-block;text-align:left;vertical-align:top><div class=c><a onclick=setmode(3)>Both</a><br><a onclick=setmode(1)>Lines</a><br><a onclick=setmode(2)>Rectangles</a></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c><input disabled style=border:0 value=ESC>Main Menu<br><input id=randomize-key maxlength=1 value='
           + settings['randomize-key'] + '>Randomize</div><hr><div class=c><label><input '
           + (settings['clear'] ? 'checked ' : '') + 'id=clear type=checkbox>Clear</label><br><input id=line-width value='
           + settings['line-width'] + '>Line Width<br><label><input '
