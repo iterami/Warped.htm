607de8b4087e33b118b6fe89deafe6a540ae2934
Jan Horava <czechinthemail@gmail.com>
2015-07-05 16:34:45 -0700

    moved buffer display to css file

--- js/warped.js
+++ js/warped.js
@@ -185,7 +185,7 @@ function setmode(newmode){
     if(mode > 0){
         save();
 
-        document.getElementById('page').innerHTML = '<canvas id=canvas></canvas><canvas id=buffer style=display:none></canvas>';
+        document.getElementById('page').innerHTML = '<canvas id=canvas></canvas><canvas id=buffer></canvas>';
 
         buffer = document.getElementById('buffer').getContext('2d');
         canvas = document.getElementById('canvas').getContext('2d');
