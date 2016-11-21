a137691b680a4024743bf86f79a22c51083122e8
honzi <czechinthemail@gmail.com>
2016-11-20 19:42:14 -0800

    made settings alter rectangles

--- js/warped.js
+++ js/warped.js
@@ -5,11 +5,30 @@ function draw_logic(){
         // Draw rectangles if not in lines mode.
         if(canvas_mode != 1){
             canvas_buffer.fillStyle = objects[object]['color'];
+
+            var height = objects[object]['x'] - mouse_x;
+            var width = objects[object]['y'] - mouse_y;
+            if(settings_settings['fixed-length'] !== 0){
+                height = settings_settings['fixed-length'];
+                width = settings_settings['fixed-length'];
+
+            }else{
+                if(settings_settings['length-multiplier'] !== 1){
+                    height *= settings_settings['length-multiplier'];
+                    width *= settings_settings['length-multiplier'];
+                }
+
+                if(settings_settings['extra-length'] !== 0){
+                    height *= settings_settings['extra-length'];
+                    width *= settings_settings['extra-length'];
+                }
+            }
+
             canvas_buffer.fillRect(
               objects[object]['x'],
               objects[object]['y'],
-              objects[object]['x'] - mouse_x,
-              objects[object]['y'] - mouse_y
+              height,
+              width
             );
         }
 
@@ -20,23 +39,23 @@ function draw_logic(){
             var target_x = mouse_x - objects[object]['x'];
             var target_y = mouse_y - objects[object]['y'];
 
-            if(settings_settings['line-fixed-length'] !== 0){
+            if(settings_settings['fixed-length'] !== 0){
                 var length = Math.sqrt(
                   target_x * target_x + target_y * target_y
                 );
 
                 target_x /= length;
-                target_x *= settings_settings['line-fixed-length'];
+                target_x *= settings_settings['fixed-length'];
                 target_y /= length;
-                target_y *= settings_settings['line-fixed-length'];
+                target_y *= settings_settings['fixed-length'];
             }
 
-            if(settings_settings['line-length-multiplier'] !== 1){
-                target_x *= settings_settings['line-length-multiplier'];
-                target_y *= settings_settings['line-length-multiplier'];
+            if(settings_settings['length-multiplier'] !== 1){
+                target_x *= settings_settings['length-multiplier'];
+                target_y *= settings_settings['length-multiplier'];
             }
 
-            if(settings_settings['line-extra-length'] !== 0){
+            if(settings_settings['extra-length'] !== 0){
                 extra_x = mouse_x - objects[object]['x'];
                 extra_y = mouse_y - objects[object]['y'];
 
@@ -45,9 +64,9 @@ function draw_logic(){
                 );
 
                 extra_x /= length;
-                extra_x *= settings_settings['line-extra-length'];
+                extra_x *= settings_settings['extra-length'];
                 extra_y /= length;
-                extra_y *= settings_settings['line-extra-length'];
+                extra_y *= settings_settings['extra-length'];
             }
 
             canvas_draw_path(
@@ -103,9 +122,9 @@ function setmode_logic(){
           + '<a onclick=canvas_setmode(1,true)>Lines</a><br><a onclick=canvas_setmode(2,true)>Rectangles</a></div></div>'
           + '<div class=right><div><input disabled value=ESC>Main Menu<br>'
           + '<input id=randomize-key maxlength=1>Randomize</div><hr>'
-          + '<div><input id=line-extra-length>Line Extra Length<br>'
-          + '<input id=line-fixed-length>Line Fixed Length<br>'
-          + '<input id=line-length-multiplier>Line Length Multiplier<br>'
+          + '<div><input id=extra-length>Extra Length<br>'
+          + '<input id=fixed-length>Fixed Length<br>'
+          + '<input id=length-multiplier>Length Multiplier<br>'
           + '<input id=line-width>Line Width<br>'
           + '<label><input id=mouse-lock type=checkbox>Mouse Lock</label><br>'
           + '<input id=number-of-objects>Objects<br>'
@@ -144,9 +163,9 @@ window.onload = function(){
     settings_init(
       'Warped.htm-',
       {
-        'line-extra-length': 0,
-        'line-fixed-length': 0,
-        'line-length-multiplier': 1,
+        'extra-length': 0,
+        'fixed-length': 0,
+        'length-multiplier': 1,
         'line-width': 1,
         'mouse-lock': true,
         'number-of-objects': 100,
