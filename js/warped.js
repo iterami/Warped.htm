cf0e60357adfcdceb61c82d39c7022765ac03c6e
honzi <czechinthemail@gmail.com>
2017-06-10 02:40:48 -0700

    replaced custom onkeydown with core keybinds

--- js/warped.js
+++ js/warped.js
@@ -6,8 +6,8 @@ function draw_logic(){
         if(canvas_mode != 1){
             canvas_buffer.fillStyle = objects[object]['color'];
 
-            var height = objects[object]['x'] - mouse_x;
-            var width = objects[object]['y'] - mouse_y;
+            var height = objects[object]['x'] - core_mouse['x'];
+            var width = objects[object]['y'] - core_mouse['y'];
             if(core_storage_data['fixed-length'] !== 0){
                 height = core_storage_data['fixed-length'];
                 width = core_storage_data['fixed-length'];
@@ -36,8 +36,8 @@ function draw_logic(){
         if(canvas_mode != 2){
             var extra_x = 0;
             var extra_y = 0;
-            var target_x = mouse_x - objects[object]['x'];
-            var target_y = mouse_y - objects[object]['y'];
+            var target_x = core_mouse['x'] - objects[object]['x'];
+            var target_y = core_mouse['y'] - objects[object]['y'];
 
             if(core_storage_data['fixed-length'] !== 0){
                 var length = Math.sqrt(
@@ -56,8 +56,8 @@ function draw_logic(){
             }
 
             if(core_storage_data['extra-length'] !== 0){
-                extra_x = mouse_x - objects[object]['x'];
-                extra_y = mouse_y - objects[object]['y'];
+                extra_x = core_mouse['x'] - objects[object]['x'];
+                extra_y = core_mouse['y'] - objects[object]['y'];
 
                 var length = Math.sqrt(
                   extra_x * extra_x + extra_y * extra_y
@@ -112,6 +112,41 @@ function randomize_objects(){
 
 function repo_init(){
     core_repo_init({
+      'keybinds': {
+        72: {
+          'todo': function(){
+              canvas_setmode({
+                'mode': 1,
+              });
+          },
+        },
+        81: {
+          'todo': canvas_menu_quit,
+        },
+      },
+      'mousebinds': {
+        'mousedown': {
+          'todo': function(){
+              if(canvas_mode <= 0
+                || core_menu_open){
+                  return;
+              }
+
+              canvas_draw();
+          },
+        },
+        'mousemove': {
+          'todo': function(){
+              if(canvas_mode <= 0
+                || (!core_storage_data['mouse-lock'] && !core_mouse['down'])
+                || core_menu_open){
+                  return;
+              }
+
+              canvas_draw();
+          },
+        },
+      },
       'storage': {
         'extra-length': 0,
         'fixed-length': 0,
@@ -119,69 +154,17 @@ function repo_init(){
         'line-width': 1,
         'mouse-lock': true,
         'number-of-objects': 100,
-        'randomize-key': 'R',
       },
       'title': 'Warped.htm',
     });
     canvas_init();
-
-    window.onkeydown = function(e){
-        if(canvas_mode <= 0){
-            return;
-        }
-
-        var key = e.keyCode || e.which;
-
-        // core_storage_data['randomize-key']: randomize current objects.
-        if(String.fromCharCode(key) === core_storage_data['randomize-key']){
-            randomize_objects();
-
-        }else if(key === 27){
-            core_escape();
-
-        }else if(key === 81){
-            canvas_menu_quit();
-        }
-    };
-
-    window.onmousedown =
-      window.ontouchstart = function(e){
-        if(canvas_mode <= 0
-          || core_menu_open){
-            return;
-        }
-
-        mouse_drag = true;
-        mouse_x = e.pageX;
-        mouse_y = e.pageY;
-
-        canvas_draw();
-    };
-
-    window.onmousemove = function(e){
-        if(canvas_mode <= 0
-          || (!core_storage_data['mouse-lock'] && !mouse_drag)
-          || core_menu_open){
-            return;
-        }
-
-        mouse_x = e.pageX;
-        mouse_y = e.pageY;
-
-        canvas_draw();
-    };
-
-    window.onmouseup = function(e){
-        mouse_drag = false;
-    };
 }
 
 function resize_logic(){
-    mouse_y = canvas_y;
-    mouse_x = canvas_x;
+    core_mouse['x'] = canvas_y;
+    core_mouse['y'] = canvas_x;
 
     canvas_buffer.lineWidth = core_storage_data['line-width'];
-    mouse_drag = false;
     randomize_objects();
 }
 
@@ -190,8 +173,7 @@ function setmode_logic(){
     if(canvas_mode === 0){
         document.getElementById('wrap').innerHTML = '<div><div><a onclick=canvas_setmode({mode:3,newgame:true})>Both</a><br>'
           + '<a onclick=canvas_setmode({mode:1,newgame:true})>Lines</a><br><a onclick=canvas_setmode({mode:2,newgame:true})>Rectangles</a></div></div>'
-          + '<div class=right><div><input disabled value=ESC>Main Menu<br>'
-          + '<input id=randomize-key maxlength=1>Randomize</div><hr>'
+          + '<div class=right><div><input disabled value=ESC>Main Menu</div><hr>'
           + '<div><input id=extra-length>Extra Length<br>'
           + '<input id=fixed-length>Fixed Length<br>'
           + '<input id=length-multiplier>Length Multiplier<br>'
@@ -207,7 +189,4 @@ function setmode_logic(){
     }
 }
 
-var mouse_drag = false;
-var mouse_x = 0;
-var mouse_y = 0;
 var objects = [];
