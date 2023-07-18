f3a6d9008e6577c71893a97083f9f71743a56458
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 15:25:04 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,28 @@
 'use strict';
 
+function load_data(){
+    canvas_setproperties({
+      'properties': {
+        'lineWidth': core_storage_data['line-width'],
+      },
+    });
+
+    let loop_counter = core_storage_data['number-of-entities'] - 1;
+    do{
+        entity_create({
+          'properties': {
+            'color': '#' + core_random_hex(),
+            'x': core_random_integer({
+              'max': canvas_properties['width'],
+            }),
+            'y': core_random_integer({
+              'max': canvas_properties['height'],
+            }),
+          },
+        });
+    }while(loop_counter--);
+}
+
 function repo_drawlogic(){
     if(core_storage_data['mouse-lock']
       || core_mouse['down-0']){
