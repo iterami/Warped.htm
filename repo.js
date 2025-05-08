6bd26f192d55e39fd7bedf7cbbcb6bdc1a8ef625
Jan Hořava <czechinthemail@gmail.com>
2025-05-08 11:15:19 -0700

    updated mouse events to pointer events

--- repo.js
+++ repo.js
@@ -22,10 +22,10 @@ function load_data(){
 }
 
 function repo_drawlogic(){
-    if(core_storage_data['mouse-lock']
-      || core_mouse['down-0']){
-        mouse_x = core_mouse['x'];
-        mouse_y = core_mouse['y'];
+    if(core_storage_data['pointer-lock']
+      || core_pointer['down-0']){
+        pointer_x = core_pointer['x'];
+        pointer_y = core_pointer['y'];
     }
 
     entity_group_modify({
@@ -38,8 +38,8 @@ function repo_drawlogic(){
                 'fillStyle': entity['color'],
               });
 
-              let height = entity['x'] - mouse_x;
-              let width = entity['y'] - mouse_y;
+              let height = entity['x'] - pointer_x;
+              let width = entity['y'] - pointer_y;
               if(core_storage_data['fixed-length'] !== 0){
                   height = core_storage_data['fixed-length'];
                   width = core_storage_data['fixed-length'];
@@ -67,8 +67,8 @@ function repo_drawlogic(){
           if(core_storage_data['mode'] !== 'rectangles'){
               let extra_x = 0;
               let extra_y = 0;
-              let target_x = mouse_x - entity['x'];
-              let target_y = mouse_y - entity['y'];
+              let target_x = pointer_x - entity['x'];
+              let target_y = pointer_y - entity['y'];
 
               if(core_storage_data['fixed-length'] !== 0){
                   const length = Math.sqrt(
@@ -87,8 +87,8 @@ function repo_drawlogic(){
               }
 
               if(core_storage_data['extra-length'] !== 0){
-                  extra_x = mouse_x - entity['x'];
-                  extra_y = mouse_y - entity['y'];
+                  extra_x = pointer_x - entity['x'];
+                  extra_y = pointer_y - entity['y'];
 
                   const length = Math.sqrt(
                     extra_x * extra_x + extra_y * extra_y
@@ -134,18 +134,18 @@ function repo_init(){
         },
       },
       'globals': {
-        'mouse_x': 0,
-        'mouse_y': 0,
+        'pointer_x': 0,
+        'pointer_y': 0,
       },
       'info': '<button id=randomize type=button>Randomize</button>',
-      'mousebinds': {
-        'mousedown': {
+      'pointerbinds': {
+        'pointerdown': {
           'todo': canvas_draw,
         },
-        'mousemove': {
+        'pointermove': {
           'todo': function(){
-              if(core_storage_data['mouse-lock']
-                || core_mouse['down-0']){
+              if(core_storage_data['pointer-lock']
+                || core_pointer['down-0']){
                   canvas_draw();
               }
           },
@@ -157,8 +157,8 @@ function repo_init(){
         'length-multiplier': 1,
         'line-width': 1,
         'mode': 'both',
-        'mouse-lock': true,
         'number-of-entities': 100,
+        'pointer-lock': true,
       },
       'storage-menu': '<table><tr><td><input class=mini id=number-of-entities min=1 step=1 type=number><td>Entities'
         + '<tr><td><input class=mini id=extra-length step=any type=number><td>Extra Length'
@@ -166,7 +166,7 @@ function repo_init(){
         + '<tr><td><input class=mini id=length-multiplier step=any type=number><td>Length Multiplier'
         + '<tr><td><input class=mini id=line-width step=any type=number><td>Line Width'
         + '<tr><td><select id=mode><option value=both>Both<option value=lines>Lines<option value=rectangles>Rectangles</select><td>Mode'
-        + '<tr><td><input id=mouse-lock type=checkbox><td>Mouse Lock</table>',
+        + '<tr><td><input id=pointer-lock type=checkbox><td>Pointer Lock</table>',
       'title': 'Warped.htm',
     });
     canvas_init({
